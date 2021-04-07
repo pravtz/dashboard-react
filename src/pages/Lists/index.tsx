dbe3a77bffbc9a15a8273e57ea3679f25b5698e2
pravtz <ederson.ufrgs@gmail.com>
2021-04-07 00:00:49 -0300

    create page

---
+++ src/pages/Lists/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import { Container, Wrapper } from './styled'
+
+const Lists = () => (
+  <Container>
+    <Wrapper>
+      <h1>This is Lists</h1>
+    </Wrapper>
+  </Container>
+)
+export default Lists

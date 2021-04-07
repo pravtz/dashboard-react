5967681c83274275c7761f1351858703e9cbbf27
pravtz <ederson.ufrgs@gmail.com>
2021-04-07 00:01:43 -0300

    create pages of the auth

---
+++ src/pages/auth/SingIn/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import { Container, Wrapper } from './styled'
+
+const SingIn = () => (
+  <Container>
+    <Wrapper>
+      <h1>This is SingIn</h1>
+    </Wrapper>
+  </Container>
+)
+export default SingIn
---
+++ src/pages/auth/SingUp/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import { Container, Wrapper } from './styled'
+
+const SingUp = () => (
+  <Container>
+    <Wrapper>
+      <h1>This is SingUp</h1>
+    </Wrapper>
+  </Container>
+)
+export default SingUp
